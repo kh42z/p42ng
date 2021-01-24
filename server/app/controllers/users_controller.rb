9fe68d176b146f08d75dad7aee3f998500a4ff82
kh42z <tterrail@student.42.fr>
2021-01-24 16:00:37 +0100

    Users route almost setup

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        json_response(@users)
+    end
+
+    def create
+        user = User.create!(user_params)
+        json_response(user, :created)
+    end
+
+    private
+    def user_params
+        params.permit(:nickname)
+    end
+
+    def set_user
+        @user = User.find(params[:user_id])
+    end
 end
