6d494278251487a679cdf068f18a6f4ebebe31b8
cbouleng <boulenge.charles28@gmail.com>
2021-01-27 19:10:06 +0100

    Following Rubocop expectancies, should we ?

--- server/app/controllers/api/ladders_controller.rb
+++ server/app/controllers/api/ladders_controller.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
 module Api
-class LaddersController < ApplicationController
-  before_action :set_ladder, only: [:show]
-  def index
-    @ladders = Ladder.all
-    json_response(@ladders)
-  end
+  class LaddersController < ApplicationController
+    before_action :set_ladder, only: [:show]
+    def index
+      @ladders = Ladder.all
+      json_response(@ladders)
+    end
 
-  def show
-    json_response(@ladder)
-  end
+    def show
+      json_response(@ladder)
+    end
 
-  private
-  def set_ladder
+    private
+
+    def set_ladder
       @ladder = Ladder.find(params[:id])
     end
-end
+  end
 end
--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -1,37 +1,40 @@
+# frozen_string_literal: true
+
 module Api
   class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
-        def index
-            @users = User.all
-            json_response(@users)
-        end
+    before_action :set_user, only: %i[show update destroy]
+    def index
+      @users = User.all
+      json_response(@users)
+    end
 
-        def create
-            user = User.create!(user_params)
-            json_response(user, :created)
-        end
+    def create
+      user = User.create!(user_params)
+      json_response(user, :created)
+    end
 
-        def update
-            @user.update(user_params)
-            json_response(@user)
-        end
+    def update
+      @user.update(user_params)
+      json_response(@user)
+    end
 
-        def show
-            json_response(@user)
-        end
+    def show
+      json_response(@user)
+    end
 
-        def destroy
-            @user.destroy
-            head :no_content
-        end
+    def destroy
+      @user.destroy
+      head :no_content
+    end
 
-   private
-        def user_params
-            params.permit(:nickname)
-        end
+    private
 
-        def set_user
-            @user = User.find(params[:id])
-        end
+    def user_params
+      params.permit(:nickname)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
   end
 end
