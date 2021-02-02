c7173bb569429a814421d228b8b3a1e9169a58c7
cbouleng <boulenge.charles28@gmail.com>
2021-02-02 13:07:16 +0100

    comment search in guild_controller

--- server/app/controllers/api/guilds_controller.rb
+++ server/app/controllers/api/guilds_controller.rb
@@ -3,7 +3,7 @@
 module Api
   class GuildsController < ApplicationController
     before_action :set_guild, only: %i[show update destroy]
-    before_action :set_guilds, only: %i[index search]
+    before_action :set_guilds, only: %i[index]
 
     def index
       json_response(@guilds)
@@ -24,24 +24,24 @@ module Api
       head :no_content
     end
 
-    def search
-      case @guilds.count
-      when 0 then render action: "no_results"
-      when 1 then render action: "show"
-      when 2..10 then render action: "show_many"
-      end
-    end
-
-    def no_results
-      # code here
-    end
+#    def search
+#      case @guilds.count
+#      when 0 then render action: "no_results"
+#      when 1 then render action: "show"
+#      when 2..10 then render action: "show_many"
+#      end
+#    end
+#
+#    def no_results
+#      # code here
+#    end
+#
+#    def show_many
+#      # code here
+#    end
 
     def show
       json_response(@guild)
-    end
-
-    def show_many
-      # code here
     end
 
     private
