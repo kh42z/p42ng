37455ae847fb8cea729f9cd04a5ee88b9ec6c25a
kh42z <tterrail@student.42.fr>
2021-02-08 15:41:53 +0100

    Faker::Boolean for 2FA

--- server/spec/factories/Users.rb
+++ server/spec/factories/Users.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.unique.name }
     image_url { Faker::Internet.url }
-    two_factor { false }
+    two_factor { Faker::Boolean.boolean }
     status { 0 }
     password { Faker::Internet.password }
     email { Faker::Internet.email }
-    mmr { Faker::Number.number(digits: 3)}
+    mmr { Faker::Number.number(digits: 3) }
     ladder
   end
 end
