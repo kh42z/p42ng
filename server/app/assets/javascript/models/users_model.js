d0d69a15fdba566e02c6504938cd27662989c2f3
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-02-10 10:12:45 +0100

    test model

---
+++ server/app/assets/javascript/models/users_model.js
@@ -0,0 +1,61 @@
+// Backbone.emulateHTTP = true
+// Backbone.emulateJSON = true
+
+const Person = Backbone.Model.extend({
+  defaults: {
+    email: 'undefined'
+  },
+
+  initialize: function () {
+    this.on('all', function (e) {
+      // this.trigger(e)
+      console.log('uid: ' + this.get('uid'))
+      console.log(this.get('name') + ' event: ' + e)
+    })
+  },
+
+  urlRoot: '/api/users/',
+  url: function () {
+    return this.urlRoot + this.id
+  },
+
+  parse: function (response) {
+    return response[2]
+  }
+})
+
+// const person = new Person({
+//   name: 'Joe Zim',
+//   age: 23
+// })
+// person.save() // POST
+// persone.destroy() // DELETE
+
+const People = Backbone.Collection.extend({
+  initialize: function () {
+    this.on('all', function (e) {
+      console.log('People event: ' + e)
+    })
+  },
+  model: Person
+})
+
+const person = new Person({ id: 1 })
+person.fetch({
+  success: function (response) {
+    console.log('Inside success')
+    console.log(response)
+    console.log('after success')
+    // console.log('parse: ' + person.parse(response))
+  },
+  error: function (errorResponse) {
+    console.log(errorResponse)
+  }
+})
+
+console.log('email: ' + person.get('email'))
+
+const people = new People()
+// people.create(person)
+
+// console.log('parse: ' + person.parse())
--- server/app/assets/javascript/templates-compiled/topnav.js
+++ server/app/assets/javascript/templates-compiled/topnav.js
@@ -1,17 +1,21 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['topnav'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+(function () {
+  const template = Handlebars.template; const templates = Handlebars.templates = Handlebars.templates || {}
+  templates.topnav = template({
+    compiler: [8, '>= 4.3.0'],
+    main: function (container, depth0, helpers, partials, data) {
+      let helper; const alias1 = depth0 != null ? depth0 : (container.nullContext || {}); const alias2 = container.hooks.helperMissing; const alias3 = 'function'; const alias4 = container.escapeExpression; const lookupProperty = container.lookupProperty || function (parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
-          return parent[propertyName];
+          return parent[propertyName]
         }
         return undefined
-    };
+      }
 
-  return "<div class=\"topnav\">\n  <a class=\"btn logo\" href=\"#home\"></a>\n  <a class=\"btn tournaments\" href=\"#tournaments\">Tournaments</a>\n  <a class=\"btn\" href=\"#leaderboard\">Leaderboard</a>\n  <a class=\"btn guilds\" href=\"#guilds\">Guilds</a>\n  <a class=\"span\"></a>\n  <a class=\"chat\" href=\"#chat\"></a>\n  <a href=\"#user_page\">\n    <img src="
-    + alias4(((helper = (helper = lookupProperty(helpers,"profile_pic") || (depth0 != null ? lookupProperty(depth0,"profile_pic") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profile_pic","hash":{},"data":data,"loc":{"start":{"line":9,"column":13},"end":{"line":9,"column":28}}}) : helper)))
-    + " class=\"profile_pic\"></img>\n    <a class=\"btn user\" href=\"#user_page\">"
-    + alias4(((helper = (helper = lookupProperty(helpers,"user") || (depth0 != null ? lookupProperty(depth0,"user") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"user","hash":{},"data":data,"loc":{"start":{"line":10,"column":42},"end":{"line":10,"column":50}}}) : helper)))
-    + "</a>\n  </a>\n  <a class=\"exit\" href=\"#exit\"></a>\n</div>\n";
-},"useData":true});
-})();
+      return '<div class="topnav">\n  <a class="btn logo" href="#home"></a>\n  <a class="btn tournaments" href="#tournaments">Tournaments</a>\n  <a class="btn" href="#leaderboard">Leaderboard</a>\n  <a class="btn guilds" href="#guilds">Guilds</a>\n  <a class="span"></a>\n  <a class="chat" href="#chat"></a>\n  <a href="#user_page">\n    <img src=' +
+    alias4(((helper = (helper = lookupProperty(helpers, 'profile_pic') || (depth0 != null ? lookupProperty(depth0, 'profile_pic') : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { name: 'profile_pic', hash: {}, data: data, loc: { start: { line: 9, column: 13 }, end: { line: 9, column: 28 } } }) : helper))) +
+    ' class="profile_pic"></img>\n    <a class="btn user" href="#user_page">' +
+    alias4(((helper = (helper = lookupProperty(helpers, 'user') || (depth0 != null ? lookupProperty(depth0, 'user') : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { name: 'user', hash: {}, data: data, loc: { start: { line: 10, column: 42 }, end: { line: 10, column: 50 } } }) : helper))) +
+    '</a>\n  </a>\n  <a class="exit" href="#exit"></a>\n</div>\n'
+    },
+    useData: true
+  })
+})()
