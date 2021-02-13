4181dfdcd6f0c61ac8ac5165eb4638ea061a7856
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-02-13 21:08:11 +0100

    create guilds tests

--- server/app/assets/javascript/views/guild/currentWarView.js
+++ server/app/assets/javascript/views/guild/currentWarView.js
@@ -1,10 +1,11 @@
 export const CurrentWarView = Backbone.View.extend({
   el: $('#app'),
   initialize: function () {
-		this.render()
+    this.render()
   },
   render: function () {
-		console.log("current war")
+    console.log('current war')
     this.$el.html('CurrentWarView')
+    return this
   }
 })
--- server/app/assets/javascript/views/header_view.js
+++ server/app/assets/javascript/views/header_view.js
@@ -8,7 +8,6 @@ export const HeaderView = Backbone.View.extend({
       user: 'pganglof',
       profile_pic: '../../images/profile-pic.jpg'
     }
-
     const templateDataTopNav = this.templateTopNav(context)
     this.$el.html(templateDataTopNav)
     return this
