f666e96c91428e672dde2d8a335cb89776895e16
kh42z <tterrail@student.42.fr>
2021-02-13 22:11:16 +0100

    add user_id to auth_origin_url callback

--- server/app/controllers/users/omniauth_callbacks_controller.rb
+++ server/app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,5 +13,18 @@ module Users
                                image_url: auth_hash['info']['image']
                              })
     end
+
+    def create_auth_params
+      @auth_params = {
+        auth_token: @token.token,
+        client_id: @token.client,
+        uid: @resource.uid,
+        expiry: @token.expiry,
+        config: @config,
+        user_id: @resource.id
+      }
+      @auth_params.merge!(oauth_registration: true) if @oauth_registration
+      @auth_params
+    end
   end
 end
