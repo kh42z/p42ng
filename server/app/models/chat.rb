165989c6bfc6acb8b07cbbe3c66f97a885aab834
cbouleng <boulenge.charles28@gmail.com>
2021-02-15 15:21:41 +0100

    rspec request on chat/chat admin & rspec model on user/guild

--- server/app/models/chat.rb
+++ server/app/models/chat.rb
@@ -4,4 +4,5 @@ class Chat < ApplicationRecord
   validates_presence_of :privacy
   belongs_to :owner, class_name: 'User'
   has_secure_password
+  has_many :chat_admins, foreign_key: 'chat_id', dependent: :destroy
 end
--- server/spec/models/chat_admin_spec.rb
+++ server/spec/models/chat_admin_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe ChatAdmin, type: :model do
   it { should validate_presence_of(:chat) }
   it { should belong_to(:user) }
   it { should belong_to(:chat) }
+
+  describe 'checks attributes' do
+    let(:user) { create(:user, nickname: "Tom") }
+    let(:chat) { create(:chat, owner: user) }
+    subject { described_class.new(chat_id: chat.id, user_id: user.id) }
+    it "expects 'TOM' to be chat admin et owner" do
+      expect(subject).to have_attributes(user_id: user.id)
+      expect(subject.chat.owner.nickname).to eq("Tom")
+    end
+  end
+
 end
