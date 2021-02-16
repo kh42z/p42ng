ae703a5d8372df16c622659188babc6be85dc399
cbouleng <boulenge.charles28@gmail.com>
2021-02-16 14:57:42 +0100

    rspec tests improved / owner is set in admin&chat controller / chat password discuss #11

--- server/db/migrate/20210208103336_create_chats.rb
+++ server/db/migrate/20210208103336_create_chats.rb
@@ -1,8 +1,8 @@
 class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
-      t.integer :privacy, :default => 0
-      t.string :password_digest, :default => ""
+      t.integer :privacy, default: 0
+      t.string :password
       t.timestamps
     end
   end
