0d6c2c49ad97221db25b407b2acf1da99e6c73b5
kh42z <tterrail@student.42.fr>
2021-02-17 17:43:23 +0100

    fix seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -15,8 +15,8 @@ State.create(name: 'In Game')
 
 if Rails.env.development?
 
-  FactoryBot.create_list(:user, 5)
-  FactoryBot.create_list(:guild_with_officers, 2)
+  FactoryBot.create_list(:user, 20)
+  FactoryBot.create_list(:guild_with_officers, 5)
   FactoryBot.create_list(:chat, 2)
 
   def chat_with_participants(count: 1)
@@ -26,9 +26,9 @@ if Rails.env.development?
   end
   chat_with_participants(count: 2)
 
-  #  (1..20).each do |id|
-#    User.find(id).update!(guild: Guild.find(rand(1..5)))
-#  end
+  (1..20).each do |id|
+    User.find(id).update!(guild: Guild.find(rand(1..5)))
+  end
 
   (1..20).each do |id|
     FactoryBot.create(:gameRecord, winner: User.find(rand(1..20)), looser: User.find(rand(1..20)))
