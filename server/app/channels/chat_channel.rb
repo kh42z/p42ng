21f8a6fc64b67d9d5c8ee2c82bcb67da05bae12f
kh42z <tterrail@student.42.fr>
2021-02-20 18:27:04 +0100

    tdd chat bans/privacy/participants

--- server/app/channels/chat_channel.rb
+++ server/app/channels/chat_channel.rb
@@ -2,23 +2,26 @@
 
 class ChatChannel < ApplicationCable::Channel
   def subscribed
+    @chat = Chat.find(params[:chat_id])
+
     return reject if reject_user?
 
-    @chat = Chat.find(params[:chat_id])
-    stream_for @chat
+    stream_for @chat if ChatParticipant.create(chat_id: @chat.id, user_id: current_user.id)
   end
 
   def received(data)
     broadcast_to(@chat, data['message'])
   end
 
-  def unsubscribed; end
+  def unsubscribed
+    ChatParticipant.where(user_id: current_user.id, chat_id: params[:chat_id]).destroy_all
+  end
 
   private
 
   def reject_user?
-    return true if ChatParticipant.where(user_id: current_user.id, chat_id: params[:chat_id]).count.zero?
     return true if ChatBan.where(user_id: current_user.id, chat_id: params[:chat_id]).count.positive?
+    return true if @chat.privacy == 2 && @chat.authenticate(params[:password]) == false
 
     false
   end
--- server/spec/channels/chat_channel_spec.rb
+++ server/spec/channels/chat_channel_spec.rb
@@ -5,27 +5,38 @@ RSpec.describe ChatChannel, type: :channel do
   let!(:current_user) { create(:user) }
 
   describe 'Participants' do
-    it "should not subscribe" do
-      stub_connection current_user: current_user
-      subscribe(chat_id: chats.first.id)
-      expect(subscription).not_to be_confirmed
-    end
-
     it "should subscribe" do
-      create(:chat_participant, user: current_user, chat: chats.first)
       stub_connection current_user: current_user
       subscribe(chat_id: chats.first.id)
       expect(subscription).to be_confirmed
+      expect(ChatParticipant.count).to eq(1)
+      unsubscribe
+      expect(ChatParticipant.count).to eq(0)
+    end
+  end
+
+  describe 'Privacy' do
+    it "should accept password" do
+      chats.last.update!(privacy: 2, password: "42")
+      stub_connection current_user: current_user
+      subscribe(chat_id: chats.last.id, password: "42")
+      expect(subscription).to be_confirmed
+    end
+
+    it "should not accept password" do
+      chats.last.update!(privacy: 2, password: "nope")
+      stub_connection current_user: current_user
+      subscribe(chat_id: chats.last.id, password: "42")
+      expect(subscription).to be_rejected
     end
   end
 
   describe 'Bans' do
     it "should not subscribe" do
-      create(:chat_participant, user: current_user, chat: chats.first)
       create(:chat_ban, user: current_user, chat: chats.first)
       stub_connection current_user: current_user
       subscribe(chat_id: chats.first.id)
-      expect(subscription).not_to be_confirmed
+      expect(subscription).to be_rejected
     end
   end
 end
