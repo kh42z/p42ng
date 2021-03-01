c058dbb5e32dbebd25782dc677f101c3f20a2561
kh42z <tterrail@student.42.fr>
2021-03-01 10:57:37 +0100

    naive implementation: email for 2FA code #18

--- server/app/controllers/users/omniauth_callbacks_controller.rb
+++ server/app/controllers/users/omniauth_callbacks_controller.rb
@@ -75,9 +75,18 @@ module Users
     private
 
     def send_code(user)
-      # code = 6.times.map{rand(10)}.join
-      # send code
+      send_mail(user)
       timer_job(user)
+    end
+
+    def send_mail(user)
+      code = 6.times.map { rand(10) }.join
+      mg_client = Mailgun::Client.new ENV['MAILGUN_SECRET']
+      message_params = { from: 'no-reply@student.42.fr',
+                         to: user.email,
+                         subject: 'Pong: Your Code!',
+                         text: "Enter this code: #{code}" }
+      mg_client.send_message(ENV['MAILGUN_DOMAIN'], message_params)
     end
 
     def timer_job(user)
