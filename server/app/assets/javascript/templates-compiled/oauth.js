ccff77ab7e5aa1d054190d8e75b61681b296c30a
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-03-03 10:10:27 +0100

    add oauth templates compiled

--- server/app/assets/javascript/templates-compiled/oauth.js
+++ server/app/assets/javascript/templates-compiled/oauth.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['oauth'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"oauth\">\n    <h1 class=\"logo\"></h1>\n    <button class=\"signIn\"\n    onclick=\"window.location.href='/auth/marvin?auth_origin_url=http://p42ng.herokuapp.com/#connexion'\"></button>\n</div>";
+    return "<div class=\"oauth\">\n    <h1 class=\"logo\"></h1>\n    <button class=\"signIn\"\n    onclick=\"window.location.href='/auth/marvin?auth_origin_url=' + window.location.href + '#connexion'\"></button>\n</div>";
 },"useData":true});
 })();
