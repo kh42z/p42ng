855774db9974c37277987af231c4244bf1401c59
kh42z <tterrail@student.42.fr>
2021-03-22 14:56:18 +0100

    User policy: better rspec coverage

--- srcs/spec/policies/user_policy_spec.rb
+++ srcs/spec/policies/user_policy_spec.rb
@@ -1,30 +1,23 @@
 require 'rails_helper'
 
 describe UserPolicy do
+  let(:auth) { create(:user ) }
+  let(:admin) { create(:user, admin: true)}
+  let(:other) { create(:user) }
   subject { described_class }
 
-  permissions :update? do
-    let(:auth) { create(:user )}
-    it "denies ban if user is not admin" do
-      expect(subject).not_to permit(auth, create(:user, banned: true))
-    end
-    it "allows ban if user is admin" do
-      expect(subject).to permit(create(:user, admin: true), create(:user, banned: true))
-    end
-  end
+  [:update?, :upload_avatar?, :create_ignore?, :destroy_ignore?, :create_friendship?, :destroy_friendship?].each do |e|
 
-
-  permissions :create_friendship? do
-    let(:auth) { create(:user )}
-    it "denies friendship if user is not admin" do
-      expect(subject).not_to permit(auth, create(:user))
-    end
-    it "allows friendship if user is admin" do
-      expect(subject).to permit(create(:user, admin: true), create(:user, banned: true))
-    end
-
-    it "allows friendship if user modifies himself" do
-      expect(subject).to permit(auth, auth)
+    permissions e do
+      it "denies #{e} if user is not admin" do
+       expect(subject).not_to permit(auth, other)
+      end
+      it "allows #{e} if user is admin" do
+       expect(subject).to permit(admin, other)
+      end
+      it "allows #{e} if user modifies himself" do
+        expect(subject).to permit(auth, auth)
+      end
     end
   end
 end
