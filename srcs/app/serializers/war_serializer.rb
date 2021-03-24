7586455a35caf4f09c9d394b49176bb8adf2cce1
cbouleng <boulenge.charles28@gmail.com>
2021-03-24 18:20:29 +0100

    war: serializer & swagger

--- srcs/app/serializers/war_serializer.rb
+++ srcs/app/serializers/war_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WarSerializer < ActiveModel::Serializer
-  attributes :id, :from, :on, :war_start, :war_end, :prize, :from_score, :on_score, :max_unanswered
-  attributes :terms_agreed, :ladder_effort, :tournament_effort, :from_agreement, :on_agreement
-  attributes :opened, :closed
+  attributes :id, :from, :on, :war_start, :war_end, :prize, :from_score,
+             :on_score, :max_unanswered, :terms_agreed, :ladder_effort, :tournament_effort,
+             :from_agreement, :on_agreement, :opened, :closed
 end
