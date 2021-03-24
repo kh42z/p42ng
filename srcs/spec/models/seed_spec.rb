46ddd5453ba5773185f8a7364d6daf9e3fe88ef4
cbouleng <boulenge.charles28@gmail.com>
2021-03-24 17:53:31 +0100

    seed: fixed

--- srcs/spec/models/seed_spec.rb
+++ srcs/spec/models/seed_spec.rb
@@ -5,6 +5,11 @@ require 'rails_helper'
 RSpec.describe Ladder, type: :model do
 
   it 'should create what I desire most' do
+    Ladder.create(name: 'Bronze', desc: 'meh.')
+    Ladder.create(name: 'Silver', desc: 'hem.')
+    Ladder.create(name: 'Gold', desc: 'ehm.')
+    Ladder.create(name: 'Platine', desc: 'mhe.')
+    Ladder.create(name: 'Diamond', desc: 'mhe.')
     guilds = FactoryBot.create_list(:guild, 5)
     guilds.each do |guild|
       @users = FactoryBot.create_list(:user, 5)
@@ -22,6 +27,9 @@ RSpec.describe Ladder, type: :model do
       guilds.without(guild).each do |on|
         FactoryBot.create(:war, from: guild, on: on, war_start: DateTime.now + rand(-5..5), war_end: DateTime.now + rand(6..10))
       end
+    end
+    User.all.each do |t|
+      t.update!(ladder_id: Ladder.all.sample.id)
     end
     expect(War.count).to eq 20
   end
